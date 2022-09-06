01b74dc357b008edccea32235cb9c9e16177fc49
Gustaf-Hasselgren <gustaf.ericson2@gmail.com>
2022-09-06 11:27:39 +0200

    Första commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,111 +5,287 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+        debugShowCheckedModeBanner: false,
+        title: 'To-do',
+        theme: ThemeData(
+          textTheme: const TextTheme(
+            bodyText2: TextStyle(fontSize: 15.0),
+          ),
+        ),
+        home: const MainView());
+  }
+}
+
+// Startsida
+class MainView extends StatelessWidget {
+  const MainView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBar(),
+      body: const TodoEntries(),
+      floatingActionButton: addEntryViewButton(context),
+    );
+  }
+
+  // Widget-metoder
+  PreferredSizeWidget appBar() {
+    return AppBar(
+        backgroundColor: Colors.blueGrey,
+        title: const Text('To-do'),
+        centerTitle: true,
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(bottom: Radius.circular(30))),
+        actions: const [FilterButton()]);
+  }
+
+  Widget addEntryViewButton(context) {
+    return FloatingActionButton(
+      backgroundColor: Colors.blueGrey,
+      child: const Icon(Icons.add_rounded, size: 50.0),
+      onPressed: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => AddEntryView()));
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+// Klass med state för FilterButton
+class FilterButton extends StatefulWidget {
+  const FilterButton({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<FilterButton> createState() => FilterButtonState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class FilterButtonState extends State<FilterButton> {
+  String? filterValue = 'All';
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return Container(
+      margin: const EdgeInsets.only(right: 18.0, top: 5.0),
+      child: DropdownButtonHideUnderline(
+        child: DropdownButton(
+          icon: const Icon(Icons.filter_alt_rounded),
+          iconSize: 30.0,
+          iconEnabledColor: Colors.white,
+          style: const TextStyle(color: Colors.black),
+          items: [filterVal('All'), filterVal('Done'), filterVal('Not done')],
+          onChanged: (String? value) {
+            setState(() {
+              filterValue = value;
+            });
+          },
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  DropdownMenuItem<String> filterVal(String value) {
+    return DropdownMenuItem<String>(value: value, child: Text(value));
+  }
+}
+
+// Stateful widget för lista med todo-entries
+
+class TodoEntries extends StatefulWidget {
+  const TodoEntries({super.key});
+
+  @override
+  State<TodoEntries> createState() => _TodoEntriesState();
+}
+
+class _TodoEntriesState extends State<TodoEntries> {
+  final entries = <String>[
+    'Rensa garderoben',
+    'Gå ut med hunden',
+    'Köpa mjölk',
+    'Posta brevet',
+    'Slackline med August',
+    'Brädspel med Mårten',
+    '01234567890123456789012345678901',
+    'hej',
+    'hej',
+    'hej',
+  ];
+
+  final isChecked = <bool>[
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          child: ListView.builder(
+            itemCount: entries.length,
+            itemBuilder: (context, int index) {
+              return entry(index);
+            },
+          ),
+        ),
+        const SizedBox(height: 90)
+      ],
+    );
+  }
+
+  // Widget-metoder
+
+  Widget entry(int index) {
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      decoration: const BoxDecoration(
+        border: Border(
+          bottom: BorderSide(color: Colors.grey),
+        ),
+      ),
+      child: Row(
+        children: [
+          entryCheckbox(index),
+          entryText(index),
+          deleteEntryButton(index),
+        ],
+      ),
+    );
+  }
+
+  Widget entryCheckbox(int index) {
+    return Checkbox(
+      fillColor: MaterialStateProperty.resolveWith(getColor),
+      value: isChecked[index],
+      onChanged: (bool? newValue) {
+        setState(
+          () {
+            isChecked[index] = newValue!;
+          },
+        );
+      },
+    );
+  }
+
+  Widget entryText(int index) {
+    return Expanded(
+      child: Text(
+        entries[index],
+        style: isChecked[index]
+            ? const TextStyle(
+                decoration: TextDecoration.lineThrough, color: Colors.grey)
+            : const TextStyle(),
+        overflow: TextOverflow.ellipsis,
+      ),
+    );
+  }
+
+  Widget deleteEntryButton(int index) {
+    return IconButton(
+      icon: const Icon(
+        Icons.delete_outline,
+        size: 30,
+      ),
+      onPressed: () {
+        deleteEntry(entries, index);
+      },
+    );
+  }
+
+  Color getColor(Set<MaterialState> states) {
+    return Colors.blueGrey;
+  }
+
+  // Funktionella metoder
+  void deleteEntry(List entryList, int index) {
+    setState(() {
+      entryList.removeAt(index);
+      isChecked.removeAt(index);
+    });
+  }
+}
+
+// Nästa view för att lägga till entries
+
+class AddEntryView extends StatelessWidget {
+  AddEntryView({super.key});
+
+  String newEntryText = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBar(),
+      body: body(),
+    );
+  }
+
+  PreferredSizeWidget appBar() {
+    return AppBar(
+        backgroundColor: Colors.blueGrey,
+        title: const Text('To-do'),
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(bottom: Radius.circular(30))),
+        centerTitle: true);
+  }
+
+  Widget body() {
+    return Container(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          Spacer(),
+          addEntryTextField(),
+          spacing(),
+          addEntryButton(),
+          Spacer()
+        ],
+      ),
+    );
+  }
+
+  // GÖR TILL STATEFUL
+
+  Widget addEntryTextField() {
+    return TextField(
+      maxLength: 31,
+      onChanged: (text) {
+        newEntryText = text;
+      },
+      decoration: const InputDecoration(
+        border: OutlineInputBorder(borderSide: BorderSide()),
+        contentPadding: EdgeInsets.all(10.0),
+        hintText: 'What are you going to do?',
+      ),
+    );
+  }
+
+  Widget addEntryButton() {
+    return TextButton.icon(
+      onPressed: () {},
+      icon: const Icon(Icons.add),
+      label: const Text('Add'),
+      style: TextButton.styleFrom(primary: Colors.blueGrey),
+    );
+  }
+
+  Widget spacing() {
+    return const SizedBox(
+      height: 10.0,
     );
   }
 }
